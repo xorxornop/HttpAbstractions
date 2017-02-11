93c6d7012b03c466b4d65f0ce6aa3f9f59b1059e
David Fowler <davidfowl@gmail.com>
2017-02-10 23:13:08 -0800

    Added support for middleware activation via IMiddlewareFactory - IMiddlewareFactory and IMiddleware are new extensiblity points for activating and authoring middleware. Under the covers, middleware is still very much just a function. This just provides a nice way to get a per request activated middleware instance that is created and released via the IMiddlewareFactory. The caveats are that middleware needs to be registered in the container (by default) and that not possible to explicitly pass arguments directly via UseMiddleware. - Added tests

---
+++ src/Microsoft.AspNetCore.Http.Abstractions/IMiddlewareFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Http
+{
+    /// <summary>
+    /// Provides methods to create middlware.
+    /// </summary>
+    public interface IMiddlewareFactory
+    {
+        /// <summary>
+        /// Creates a middleware instance for each request.
+        /// </summary>
+        /// <param name="middlewareType">The concrete <see cref="Type"/> of the <see cref="IMiddleware"/>.</param>
+        /// <returns>The <see cref="IMiddleware"/> instance.</returns>
+        IMiddleware Create(Type middlewareType);
+
+        /// <summary>
+        /// Releases a <see cref="IMiddleware"/> instance at the end of each request.
+        /// </summary>
+        /// <param name="middleware">The <see cref="IMiddleware"/> instance to release.</param>
+        void Release(IMiddleware middleware);
+    }
+}
---
+++ src/Microsoft.AspNetCore.Http/MiddlewareFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public class MiddlewareFactory : IMiddlewareFactory
+    {
+        // The default middleware factory is just an IServiceProvider proxy.
+        // This should be registered as a scoped service so that the middleware instances
+        // don't end up being singletons.
+        private readonly IServiceProvider _serviceProvider;
+
+        public MiddlewareFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IMiddleware Create(Type middlewareType)
+        {
+            return _serviceProvider.GetRequiredService(middlewareType) as IMiddleware;
+        }
+
+        public void Release(IMiddleware middleware)
+        {
+            // The container owns the lifetime of the service
+        }
+    }
+}
