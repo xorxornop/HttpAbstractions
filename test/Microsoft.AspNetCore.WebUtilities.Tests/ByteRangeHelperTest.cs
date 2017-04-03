9f29dcfcb8d53447f04c4250f8400a9c0968ea8f
Jass Bagga <jbagga93@live.ca>
2017-04-03 15:42:04 -0700

    Sort usings

--- test/Microsoft.AspNetCore.WebUtilities.Tests/ByteRangeHelperTest.cs
+++ test/Microsoft.AspNetCore.WebUtilities.Tests/ByteRangeHelperTest.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Net.Http.Headers;
 using System.Collections.Generic;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 
 namespace Microsoft.AspNetCore.WebUtilities
