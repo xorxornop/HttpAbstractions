640e0acb5c7ae5ce1e75185b1951882766a887eb
Jass Bagga <jbagga93@live.ca>
2017-04-06 11:47:36 -0700

    Add necessary ProjectReferences

--- src/Microsoft.AspNetCore.WebUtilities/ByteRangeHelper.cs
+++ src/Microsoft.AspNetCore.WebUtilities/ByteRangeHelper.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
 
